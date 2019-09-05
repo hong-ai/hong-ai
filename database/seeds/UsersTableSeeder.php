981f10bd6a2a3aab8ae22539bee75a1d34a86e9e
vagrant <vagrant@homestead>
2019-09-05 07:43:28 +0000

    内容管理后台

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -40,8 +40,8 @@ class UsersTableSeeder extends Seeder
 
         // 单独处理第一个用户的数据
         $user = User::find(1);
-        $user->name = 'Summer';
-        $user->email = 'summer@example.com';
+        $user->name = 'hqz';
+        $user->email = '1402888342@qq.com';
         $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
 
